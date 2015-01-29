02c064f85f3b38b747afd5ef59cf90eebc026d27
BerliozLeChat <neoflash93@gmail.com>
2015-01-29 16:09:10 +0000

    ajout fonctions de conversion nat2

--- TP1/tp_nat2.ml
+++ TP1/tp_nat2.ml
@@ -8,3 +8,30 @@
 
 #load "nat_2.cmo";;
 open Nat_2;;
+
+(*
+nat_of_int :  int -> naturel
+Convertit un int en naturel
+*)
+let rec nat_of_int(x) =
+	if x=0 then
+		zero()
+	else if x=1 then
+		un()
+	else
+		sucsuc(nat_of_int(x-2));;
+
+(*
+int_of_nat : naturel -> int
+Convertit un naturel en int
+*)
+let rec int_of_nat(x) =
+	if est_zero(x) then
+		0
+	else if est_un(x) then
+		1
+	else
+		2+int_of_nat(prepre(x));;
+
+int_of_nat(nat_of_int(int_of_nat(zero())));;
+int_of_nat(nat_of_int(5));;
