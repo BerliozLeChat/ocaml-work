cd078a199cad69f5488c50a1486b24772d6e4ed3
BerliozLeChat <neoflash93@gmail.com>
2015-01-29 14:23:32 +0000

    ajout de fonctions dans nat1

--- TP1/tp_nat1.ml
+++ TP1/tp_nat1.ml
@@ -5,8 +5,40 @@
 * Licence 3 Informatique
 * Université de Nantes
 *)
+
 #load "nat_1.cmo";;
 open Nat_1;;
+
+(*
+plus : naturel*naturel -> naturel
+opérateur d'addition entre deux naturels
+*)
+let rec plus(a,b) =
+	if est_zero(b) then
+	a
+	else
+	plus(suc(a),pre(b));;
+
+(*
+fois : naturel*naturel -> naturel
+opérateur de multiplication entre deux naturels
+*)	
+let rec fois(a,b) =
+	if est_zero(b) then
+		zero()
+	else
+	plus(a,fois(a,pre(b)));;
+	
+
+(*
+nat_of_int :  int -> naturel
+Convertit un int en naturel
+*)
+let rec nat_of_int(x) =
+	if x=0 then
+	zero()
+	else
+	suc(nat_of_int(x-1));;
 
 (*
 int_of_nat : naturel -> int
@@ -19,5 +51,6 @@ let rec int_of_nat(x) =
 	1+int_of_nat(pre(x));;
 	
 	
-int_of_nat(suc(suc(zero())));;
+int_of_nat(nat_of_int(int_of_nat(zero())));;
+int_of_nat(fois(suc(suc(zero())),suc(suc(zero()))));;
 
